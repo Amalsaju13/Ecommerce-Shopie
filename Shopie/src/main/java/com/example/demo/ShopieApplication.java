2a1e924fd7ce1ccfe0cc21f2f0b487a465e253a8
Amalsaju13 <92660324+Amalsaju13@users.noreply.github.com>
2022-11-28 22:01:46 +0530

    Add files via upload

---
+++ Shopie/src/main/java/com/example/demo/ShopieApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopieApplication.class, args);
+	}
+
+}
